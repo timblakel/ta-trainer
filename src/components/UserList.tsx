da542ebfababa6ccb1bf1beb6617f7de5bfc37d2
Austin Cory Bart <acbart@vt.edu>
2021-09-20 01:30:02 -0400

    Created editable users list

---
+++ src/components/UserList.tsx
@@ -0,0 +1,16 @@
+import EditableDndList, { Task } from "editable-dnd-list";
+import { useState } from "react";
+
+
+
+export function UserList(): JSX.Element {
+  const [users, setUsers] = useState<Task[]>([
+    {id: '1', text: "Dr. Bart"}
+  ]);
+
+  function handleChange(items: Task[]): void {
+    console.log(items);
+  }
+
+  return <EditableDndList items={users} onChange={handleChange} />;
+}
