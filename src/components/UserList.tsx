1b35e13f98b7547bdd828bbb369cc07d1f9456aa
Austin Cory Bart <acbart@vt.edu>
2021-09-20 01:56:46 -0400

    Clean up codebase of dead imports and lazy code

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,6 +1,4 @@
 import EditableDndList, { Task } from "editable-dnd-list";
-import { useState } from "react";
-
 
 
 export function UserList({users, setUsers}: {users: Task[], setUsers: (t: Task[])=>void}): JSX.Element {
