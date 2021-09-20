58ffa0fac5b2d808c17e6cfa024de00fa0499af5
Austin Cory Bart <acbart@vt.edu>
2021-09-20 01:04:25 -0400

    Swappable cards!

--- src/utilities/data.ts
+++ src/utilities/data.ts
@@ -1,3 +1,9 @@
+// https://stackoverflow.com/a/5915122/1718155
+/**
+ * Grab a random element from a list
+ * @param items Any kind of list 
+ * @returns One randomly chosen element
+ */
 export function getRandomElement<T>(items: T[]): T {
   return items[Math.floor(Math.random()*items.length)];
 }
